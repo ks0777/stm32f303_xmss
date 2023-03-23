9fc3a937ba4b376a429d80751878dbfd95581d41
Kevin Schneider <kevin.schneider@tum.de>
2023-03-23 15:21:49 +0100

    idk

--- Inc/trigger.h
+++ Inc/trigger.h
@@ -7,7 +7,7 @@
 
 #define TRIGGER_PORT		GPIOC
 #define TRIGGER_PIN 		GPIO_PIN_13
-#define TRIGGER_CLK_ENABLE	__HAL_RCC_GPIOA_CLK_ENABLE
+#define TRIGGER_CLK_ENABLE	__HAL_RCC_GPIOC_CLK_ENABLE
 
 void trigger_init(void);
 void trigger_set(bool state);
